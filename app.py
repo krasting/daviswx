c7eda8897df0d7e79667787efccec930d1241a55
John Krasting <krasting@gmail.com>
2019-12-14 21:32:19 -0500

    Sample flask app updates
    - optional hostname/port specification
    - remove double execution of scheduled job

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_apscheduler import APScheduler
 import datetime
+import os
 import time
 
 app = Flask(__name__)
@@ -10,18 +11,25 @@ import daviswx
 O = daviswx.getobs.current()
 time.sleep(10)
 
+
 #function executed by scheduled job
-def my_job(text):
+def my_job(hostname,port):
     global O
     O = daviswx.getobs.current()
 
+
 @app.route('/')
 def hello():
-    msg = "Hello World!  The current temperature is: "+str(O.rtOutsideTemp)+' at '+O.rtObsTime.strftime('%Y-%m-%d %H:%M' )
+    msg = "Hello World!  The current temperature is: "+str(O.rtOutsideTemp)+\
+        ' at '+O.rtObsTime.strftime('%Y-%m-%d %H:%M' )
     return msg
 
 if __name__ == '__main__':
     scheduler = APScheduler()
-    scheduler.add_job(func=my_job, args=['job run'], trigger='interval', id='job', seconds=300)
+    hostname = os.environ['DAVIS_HOSTNAME']
+    port = os.environ['DAVIS_PORT']
+    scheduler.add_job(func=my_job, args=[hostname,port], id='job', \
+        trigger='cron', minute='*/5')
     scheduler.start()
-    app.run("0.0.0.0", port=80, debug=True)
+    # reloader == False below to prevent double-execution of scheduler jobs
+    app.run("0.0.0.0", port=80, debug=True, use_reloader=False)
