efd09b6a34d4072a90c86ca2084429c66959e8a0
John Krasting <krasting@gmail.com>
2019-12-16 21:06:32 -0500

    Mods to sample app.py
    Prints pandas dataframe of observations

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_apscheduler import APScheduler
 import datetime
 import os
+import pandas as pd
 import time
 
 app = Flask(__name__)
@@ -9,19 +10,28 @@ app = Flask(__name__)
 # get current obs
 import daviswx
 O = daviswx.getobs.current()
+df = pd.DataFrame(O.__dict__,index=[O.rtObsTime,])
 time.sleep(10)
 
+def filter_dataframe(df):
+    onedayago = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
+    df = df[df.index >= onedayago]
+    df = df.drop_duplicates()
+    return df
 
 #function executed by scheduled job
 def my_job(hostname,port):
     global O
+    global df
     O = daviswx.getobs.current()
-
+    df = df.append(pd.DataFrame(O.__dict__,index=[O.rtObsTime,]))
+    df = filter_dataframe(df)
 
 @app.route('/')
 def hello():
     msg = "Hello World!  The current temperature is: "+str(O.rtOutsideTemp)+\
         ' at '+O.rtObsTime.strftime('%Y-%m-%d %H:%M' )
+    msg = df[['rtOutsideTemp','rtOutsideHum']].to_html()
     return msg
 
 if __name__ == '__main__':
