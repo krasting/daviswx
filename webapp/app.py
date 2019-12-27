4ddf893419430343acedc70334b6584e34ceaf11
John Krasting <krasting@gmail.com>
2019-12-26 21:15:27 -0500

    Moved figure updating to the scheduler job

--- webapp/app.py
+++ webapp/app.py
@@ -17,8 +17,47 @@ import matplotlib.dates as mdates
 
 register_matplotlib_converters()
 plt.rcParams.update({'font.size': 12})
+matplotlib.rcParams['timezone'] = 'US/Eastern'
 
 app = Flask(__name__)
+
+def make_plot(x):
+    _x = x.copy()
+    _x = convert_timezone(_x)
+    fig = plt.figure(figsize=(8,4.5))                                                                                 
+    ax = plt.subplot(1,1,1)
+    # plot the data
+    plt.plot(_x.index,_x.rtOutsideTemp,'b',linewidth=2)
+    # add title and format tick labels
+    ax.text(0.02,1.02,'Temperature',transform=ax.transAxes)
+    myFmt = mdates.DateFormatter('%H:%M')
+    ax.xaxis.set_major_formatter(myFmt)
+    # add grid lines
+    plt.grid(True)
+    # save the figure and return the text
+    imgdata = io.BytesIO()
+    plt.savefig(imgdata,bbox_inches='tight')
+    uri = 'data:image/png;base64,' + base64.b64encode(imgdata.getvalue()).decode('utf-8').replace('\n', '')
+    plt.close('all')
+    del fig
+    del imgdata
+    return '<img src = "%s"/>' % uri
+
+def filter_dataframe(df):
+    onedayago = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
+    df = df[df.index >= onedayago]
+    df = df.drop_duplicates()
+    df = df.sort_values(by='rtObsTime',ascending=False)
+    df.to_pickle('./obs_dataframe.pkl')
+    return df
+
+def convert_timezone(x):
+    dti = x.index
+    dti = dti.tz_localize(datetime.timezone.utc)
+    dti = dti.tz_convert('US/Eastern')
+    _x = x.copy()
+    _x.index = dti
+    return _x
 
 # setup a testing mode
 if os.path.exists('./test_data.pkl'):
@@ -36,57 +75,27 @@ if production_mode is True:
     time.sleep(10)
 else:
     df = pd.read_pickle('./test_data.pkl')
-
-def make_plot(df):
-    fig = plt.figure(figsize=(8,4.5))                                                                                 
-    ax = plt.subplot(1,1,1)
-    # plot the data
-    plt.plot(df.index,df.rtOutsideTemp,'b',linewidth=2)
-    # add title and format tick labels
-    ax.text(0.02,1.02,'Temperature',transform=ax.transAxes)
-    myFmt = mdates.DateFormatter('%H:%M')
-    ax.xaxis.set_major_formatter(myFmt)
-    # add grid lines
-    plt.grid(True)
-    # save the figure and return the text
-    imgdata = io.BytesIO()
-    plt.savefig(imgdata,bbox_inches='tight')
-    uri = 'data:image/png;base64,' + base64.b64encode(imgdata.getvalue()).decode('utf-8').replace('\n', '')
-    return '<img src = "%s"/>' % uri
-
-def filter_dataframe(df):
-    onedayago = datetime.datetime.utcnow() - datetime.timedelta(hours=24)
-    df = df[df.index >= onedayago]
-    df = df.drop_duplicates()
-    df = df.sort_values(by='rtObsTime',ascending=False)
-    df.to_pickle('./obs_dataframe.pkl')
-    return df
-
-def convert_timezone(df):
-    dti = df.index
-    dti = dti.tz_localize(datetime.timezone.utc)
-    dti = dti.tz_convert('US/Eastern')
-    df.index = dti
-    return df
+img = make_plot(df)
 
 #function executed by scheduled job
 def my_job(hostname,port):
     global O
     global df
+    global img
     O = daviswx.getobs.current()
     df = df.append(pd.DataFrame(O.__dict__,index=[O.rtObsTime,]))
     df = filter_dataframe(df)
+    img = make_plot(df)
 
 @app.route('/')
 def hello():
-    df_tz = convert_timezone(df[['rtOutsideTemp','rtOutsideHum']])
     return render_template('current.html', \
              temp=str(O.rtOutsideTemp), \
              humidity=str(O.rtOutsideHum), \
              barometer=str(O.rtBaroCurr), \
              raintoday=str(O.rtDayRain), \
-             plot=make_plot(df_tz),\
-             dataframe = df_tz.to_html())
+             plot=img,\
+             dataframe = df[['rtOutsideTemp','rtOutsideHum']].to_html())
 
 if __name__ == '__main__':
     if production_mode is True:
